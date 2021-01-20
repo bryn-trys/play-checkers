a7d408c12cb44bbf89476906da56b44570139c00
Bryn <63167432+bryn-trys@users.noreply.github.com>
2021-01-20 12:25:37 -0800

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,23 @@
+html {
+	background-color: aliceblue;
+	font-family: 'Lato', sans-serif;
+}
+
+.white {
+	width: 40px;
+	height: 40px;
+	border: 4px solid black;
+	background-color: whitesmoke;
+	float: left;
+}
+.red {
+	width: 40px;
+	height: 40px;
+	border: 4px solid black;
+	background-color: firebrick;
+	float: left;
+}
+#board {
+	width: 384px;
+	height: 384px;
+}
